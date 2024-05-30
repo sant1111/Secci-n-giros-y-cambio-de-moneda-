b7774b1b9c88f9bd58e622e40fc9c08b7e935a82
Santiago <santyfoca15@gmail.com>
2024-05-30 12:31:33 -0500

    Se añadio la clase persona

--- src/giros/y/cambio/de/moneda/Persona.java
+++ src/giros/y/cambio/de/moneda/Persona.java
@@ -9,4 +9,53 @@ public class Persona {
     private int numeroTelefono;
     private String correoElectronico;
     private String DireccionResidencia;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
+
+    public void setNumeroIdentificacion(int numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(int numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccionResidencia() {
+        return DireccionResidencia;
+    }
+
+    public void setDireccionResidencia(String DireccionResidencia) {
+        this.DireccionResidencia = DireccionResidencia;
+    }
+    
 }
