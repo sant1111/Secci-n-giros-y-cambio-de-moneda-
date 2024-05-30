16e26db1bb0f96493ae2ee28d00339a337e61b04
Santiago <santyfoca15@gmail.com>
2024-05-30 11:10:17 -0500

    agrege un sout

--- src/giros/y/cambio/de/moneda/Principal.java
+++ src/giros/y/cambio/de/moneda/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
  
     public static void main(String[] args) {
         System.out.println("Hola mundo ");
-        System.out.println(" Haciendo un cambio en la rama santiago");
+        System.out.println("xd");
     }
     
 }
