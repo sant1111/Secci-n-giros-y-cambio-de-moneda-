f04f912eafb1268c1349164a5889e5555dcd2b2c
Santiago <santyfoca15@gmail.com>
2024-05-30 12:30:40 -0500

    Se añadio la clase persona

---
+++ src/giros/y/cambio/de/moneda/Persona.java
@@ -0,0 +1,12 @@
+
+package giros.y.cambio.de.moneda;
+
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private int numeroIdentificacion;
+    private int numeroTelefono;
+    private String correoElectronico;
+    private String DireccionResidencia;
+}
